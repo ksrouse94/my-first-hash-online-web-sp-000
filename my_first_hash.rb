26cc122a30ecc7ee6ff1d82e46d588192c6c2742
Kimberly Rouse <ksrouse94@gmail.com>
2019-03-06 01:17:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"my name" = "Kim Rouse"}
+  your_hash = {"my name" => "Kim Rouse", "age" => 25}
   
 end
 
